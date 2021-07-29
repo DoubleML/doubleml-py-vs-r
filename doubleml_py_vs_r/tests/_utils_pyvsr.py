b0611f998cd7a93fa44bf2adc24d206ec6a9ac5f
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-07-29 17:55:26 +0200

    added some py vs r tests for clustered standard errors

--- doubleml_py_vs_r/tests/_utils_pyvsr.py
+++ doubleml_py_vs_r/tests/_utils_pyvsr.py
@@ -48,6 +48,7 @@ r_MLPLR = robjects.r('''
             double_mlplr_obj$set_sample_splitting(smpls)
 
             double_mlplr_obj$fit()
+            print(data_ml)
             return(list(coef = double_mlplr_obj$coef,
                         se = double_mlplr_obj$se))
         }
@@ -256,5 +257,58 @@ r_IIVM = robjects.r('''
             double_mliivm_obj$fit()
             return(list(coef = double_mliivm_obj$coef,
                         se = double_mliivm_obj$se))
+        }
+        ''')
+
+r_MLPLIV_multiway_cluster = robjects.r('''
+        library('DoubleML')
+        library('mlr3learners')
+        library('data.table')
+        library('mlr3')
+
+        f <- function(data, score, dml_procedure,
+                      train_ids, test_ids,
+                      cluster_var1, cluster_var2=NULL) {
+            data = data.table(data)
+            mlmethod_g = 'regr.lm'
+            mlmethod_m = 'regr.lm'
+            mlmethod_r = 'regr.lm'
+
+            if (is.null(cluster_var2)) cluster_vars = cluster_var1 else cluster_vars = c(cluster_var1, cluster_var2)
+            Xnames = names(data)[names(data) %in% c("Y", "D", "Z", cluster_vars) == FALSE]
+            data_ml = double_ml_data_from_data_frame(data, y_col = "Y",
+                                                     d_cols = "D", x_cols = Xnames,
+                                                     z_col = "Z",
+                                                     cluster_cols = cluster_vars)
+
+            double_mlpliv_obj = DoubleMLPLIV$new(data_ml,
+                                                 n_folds = 2,
+                                                 ml_g = mlmethod_g,
+                                                 ml_m = mlmethod_m,
+                                                 ml_r = mlmethod_r,
+                                                 dml_procedure = dml_procedure,
+                                                 score = score)
+
+            smpls = list(list(train_ids=train_ids, test_ids=test_ids))
+            smpls_cluster = list(train_ids = list(), test_ids = list())
+            for (i_smpl in 1:length(train_ids)) {
+                this_cluster_smpl_train = list()
+                this_cluster_smpl_test = list()
+                for (i_var in 1:length(cluster_vars)) {
+                    this_cluster_var = data[[cluster_vars[i_var]]]
+                    train_clusters = unique(this_cluster_var[train_ids[[i_smpl]]])
+                    test_clusters = unique(this_cluster_var[test_ids[[i_smpl]]])
+                    this_cluster_smpl_train[[i_var]] = train_clusters
+                    this_cluster_smpl_test[[i_var]] = test_clusters
+                }
+                smpls_cluster$train_ids[[i_smpl]] = this_cluster_smpl_train
+                smpls_cluster$test_ids[[i_smpl]] = this_cluster_smpl_test
+            }
+            double_mlpliv_obj$.__enclos_env__$private$smpls_ = smpls
+            double_mlpliv_obj$.__enclos_env__$private$smpls_cluster_ = list(smpls_cluster)
+
+            double_mlpliv_obj$fit()
+            return(list(coef = double_mlpliv_obj$coef,
+                        se = double_mlpliv_obj$se))
         }
         ''')
